9564118ff7385c74f3b3b5a1f217ab4b6604a4f0
xjapanseikima <magican213@gmail.com>
2021-11-06 18:14:20 -0400

    test with bootstrap angular ngb carousel

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent implements OnInit {
-
-  constructor() { }
+  images = [700, 800, 807].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   ngOnInit(): void {
   }
-
 }
