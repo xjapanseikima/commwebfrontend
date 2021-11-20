ebf92f7174106059a9c2d80227ca7d20607ac206
xjapanseikima <magican213@gmail.com>
2021-11-20 16:47:38 -0500

    add , into branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    SliderComponent
+    SliderComponent,
     FooterComponent
   ],
   imports: [MatToolbarModule,
